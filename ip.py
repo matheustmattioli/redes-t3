af1bdf6d786fe50bb0d703b5b3387906895ecf0f
Matheus Mattioli <matheus.mattioli@hotmail.com>
2022-03-21 22:15:51 -0300

    passo 1 impl
    Implementamos o passo 1 e demos início ao passo 2
    
    Co-Authored-By: Gabriel Penajo <55462743+gabrielpenajo@users.noreply.github.com>
    Co-Authored-By: Isabela V. Magalhães <50814645+isarbela@users.noreply.github.com>

--- ip.py
+++ ip.py
@@ -1,3 +1,4 @@
+from grader.tcputils import calc_checksum
 from iputils import *
 
 
@@ -9,6 +10,7 @@ class IP:
         Ethernet com ARP).
         """
         self.callback = None
+        self.identification = 0
         self.enlace = enlace
         self.enlace.registrar_recebedor(self.__raw_recv)
         self.ignore_checksum = self.enlace.ignore_checksum
@@ -31,15 +33,17 @@ class IP:
         # TODO: Use a tabela de encaminhamento para determinar o próximo salto
         # (next_hop) a partir do endereço de destino do datagrama (dest_addr).
         # Retorne o next_hop para o dest_addr fornecido.
-        MASK = (0xffffffff << 8)
 
-        net = struct.unpack('!I', str2addr(self.tabela[0][-1]))[0]
-        dest_addr_number = struct.unpack('!I', str2addr(dest_addr))[0]
-        print(net & MASK, dest_addr_number & MASK)
-        if (net & MASK) == (dest_addr_number & MASK):
-            print(net)
+        MASKSIZE = 32
 
-        return None
+        for entrada in self.tabela:
+            net = struct.unpack('!I', str2addr(entrada[0].split('/')[0]))[0]
+            offset = int(entrada[0].split('/')[1])
+            dest_addr_number = struct.unpack('!I', str2addr(dest_addr))[0]
+            MASK = (0xffffffff << (MASKSIZE - offset))
+            if (net & MASK) == (dest_addr_number & MASK): 
+                return entrada[1]
+
 
     def definir_endereco_host(self, meu_endereco):
         """
@@ -61,8 +65,7 @@ class IP:
         # converta-a em uma estrutura de dados mais eficiente.
         self.tabela = tabela
         return
-        self.tabela = dict()
-
+    
     def registrar_recebedor(self, callback):
         """
         Registra uma função para ser chamada quando dados vierem da camada de rede
@@ -77,4 +80,34 @@ class IP:
         next_hop = self._next_hop(dest_addr)
         # TODO: Assumindo que a camada superior é o protocolo TCP, monte o
         # datagrama com o cabeçalho IP, contendo como payload o segmento.
+
+        # datagrama = IP + SEGMENTO
+        # construir o header IP e no final concatenar com segmento
+
+        # https://en.wikipedia.org/wiki/IPv4#Header
+
+        # Algo nesse sentido, tem q arrumar algumas coisas ainda
+        checksum = 0
+        protocol = 6
+        ttl = 64
+        flags = 0x0
+        frag_offset = 0x0
+        total_len = 20 + len(segmento)
+        ecn = 0x0
+        dcsp = 0x0 
+        version = 0x4
+        ihl = 0x5
+        self.identification += 1
+
+        ip_header = struct.pack('!BBHHHBBH', version, ihl, dcsp, ecn, total_len, self.identification, flags, \
+            frag_offset, ttl, protocol, checksum, self.meu_endereco, dest_addr)
+        ip_header += self.meu_endereco + dest_addr
+
+        checksum = calc_checksum(ip_header)
+        ip_header = struct.pack('!BBHHHBBH', version, ihl, dcsp, ecn, total_len, self.identification, flags, \
+            frag_offset, ttl, protocol, checksum)
+        ip_header += self.meu_endereco + dest_addr
+
+
+        datagrama = ip_header + segmento
         self.enlace.enviar(datagrama, next_hop)
